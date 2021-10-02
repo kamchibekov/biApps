0b377945ba3559ce9758e08ef3493fc8433f422c
Adilet Kamchibekov <adiletkamchibekov@gmail.com>
2021-10-02 17:25:43 +0900

    logo changed

--- public/js/custom.js
+++
@@ -1,7 +0,0 @@
-$(".navbar-dark .navbar-nav .nav-link").click(function () {
-    var id = $(this).attr('href');
-    if (id.length > 1)
-        $('html, body').animate({
-            scrollTop: $(id).offset().top - 10
-        }, 1000);
-});
